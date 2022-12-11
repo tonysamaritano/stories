9cba9ea8a513efc58bca3ec2ee8f91d7769c1c12
Tony Samaritano <tony@vergeaero.com>
2022-12-11 14:55:35 -0500

    adds database

---
+++ database.py
@@ -0,0 +1,128 @@
+import os
+import datetime
+
+from sqlalchemy import Boolean, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy.orm import sessionmaker, relationship
+
+engine = create_engine(os.environ.get("DATABASE_URL"),
+                       connect_args={"check_same_thread": False}, echo=False)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# class Simulation(Base):
+#     __tablename__ = 'simulations'
+
+#     id = Column(Integer, primary_key=True)
+#     uuid = Column(String, unique=True)
+#     created = Column(DateTime, default=datetime.datetime.utcnow)
+#     type = Column(String)
+#     owner = Column(String)
+
+#     logs = relationship("Log", back_populates="simulation")
+#     reports = relationship("Report", back_populates="simulation")
+
+#     vbake_id = Column(Integer, ForeignKey("vbakes.id"))
+#     vbake = relationship("VBake", uselist=False)
+
+#     vpkg_id = Column(Integer, ForeignKey("vpkgs.id"))
+#     vpkg = relationship("VPackage", uselist=False)
+
+#     config_id = Column(Integer, ForeignKey("configs.id"))
+#     config = relationship("Configuration", uselist=False)
+
+
+# class Log(Base):
+#     __tablename__ = 'logs'
+
+#     id = Column(Integer, primary_key=True)
+#     simulation_id = Column(Integer, ForeignKey("simulations.id"))
+#     log = Column(String)
+#     type = Column(String)
+
+#     simulation = relationship("Simulation", back_populates="logs")
+
+
+# class Report(Base):
+#     __tablename__ = 'reports'
+
+#     id = Column(Integer, primary_key=True)
+#     simulation_id = Column(Integer, ForeignKey("simulations.id"))
+#     report = Column(String)
+#     type = Column(String)
+
+#     simulation = relationship("Simulation", back_populates="reports")
+
+
+# class VBake(Base):
+#     __tablename__ = 'vbakes'
+
+#     id = Column(Integer, primary_key=True)
+#     owner = Column(String)
+#     vbake = Column(String)
+
+
+# class VPackage(Base):
+#     __tablename__ = 'vpkgs'
+
+#     id = Column(Integer, primary_key=True)
+#     vpkg = Column(String)
+#     owner = Column(String)
+
+
+# class Configuration(Base):
+#     __tablename__ = 'configs'
+
+#     id = Column(Integer, primary_key=True)
+#     config = Column(String)
+#     owner = Column(String)
+
+class Pet(Base):
+    __tablename__ = 'pet'
+
+    id = Column(Integer, primary_key=True)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", uselist=False)
+
+    name = Column(String)
+    breed = Column(String)
+    gender = Column(String)
+    photo = Column(String, default=None)
+
+
+class StoryRequest(Base):
+    __tablename__ = 'story_requests'
+
+    id = Column(Integer, primary_key=True)
+    pet_id = Column(Integer, ForeignKey("pet.id"))
+    pet = relationship("Pet", uselist=False)
+
+    owner = Column(String)
+
+    adj_0 = Column(String)
+    adj_1 = Column(String)
+    adj_2 = Column(String)
+
+    backstory = Column(String, default=None)
+    details = Column(String, default=None)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    password = Column(String, default="none")
+    first_name = Column(String)
+    last_name = Column(String)
+    picture = Column(String)
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    disabled = Column(Boolean, default=False)
+    tokens = Column(Integer)
+
+
+Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from unittest import result
 from fastapi import FastAPI, Cookie, HTTPException, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,6 +14,9 @@ from jose import jwt
 
 import stripe
 
+import database as db
+
+
 stripe.api_key = "sk_test_51MDdH0DrFDmQr8e1pI97uj8lleQCqgPOLrk7EcoYBLi6O27M3mg5VpLaBJwfem67b1DGFjMRe9cCNPemwQJZ0Jwt00pEaGiAy7"
 CLIENT_ID = "294622490933-iglmkvhlsk9dmucq0etdp0tnkeqfj54j.apps.googleusercontent.com"
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -23,23 +27,54 @@ class Token(BaseModel):
     token: str
 
 
-class StoryRequest(BaseModel):
-    name: str
-    breed: str
-    gender: str
-    adj_0: str
-    adj_1: str
-    adj_2: str
-    backstory: Optional[str] = None
-    details: Optional[str]
-
-
 class User(BaseModel):
     email: str
     first_name: str
     last_name: str
     picture: str
     tokens: int
+
+    class Config:
+        orm_mode = True
+
+
+class PetRequest(BaseModel):
+    name: str
+    breed: str
+    gender: str
+    photo: Optional[str]
+
+
+class PetPatch(BaseModel):
+    name: Optional[str]
+    breed: Optional[str]
+    gender: Optional[str]
+    photo: Optional[str]
+
+
+class Pet(PetRequest):
+    id: int
+    owner: User
+
+    class Config:
+        orm_mode = True
+
+
+class StoryRequest(BaseModel):
+    adj_0: str
+    adj_1: str
+    adj_2: str
+    backstory: Optional[str] = None
+    details: Optional[str] = None
+
+
+class Story(StoryRequest):
+    id: int
+    pet: Pet
+    owner: str
+
+    class Config:
+        orm_mode = True
 
 
 fake_users_db = {
@@ -73,32 +108,130 @@ async def me(bearer: Optional[str] = Cookie(None)):
     except:
         raise HTTPException(status_code=401, detail="Not Logged In")
 
-    if user["email"] not in fake_users_db:
-        fake_users_db[user["email"]] = 0
+    result = db.session.query(db.User).filter(
+        db.User.email == str(user["email"])).first()
 
-    return User(tokens=fake_users_db[user["email"]], **user)
+    assert result is not None
+
+    return User.from_orm(result)
 
 
-@app.post("/story")
-async def story(story: StoryRequest, bearer: Optional[str] = Cookie(None)):
-    print(story)
+@app.post("/{pet_id}/story")
+async def story(pet_id: int, story: StoryRequest, bearer: Optional[str] = Cookie(None)):
+    user = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
 
-    try:
-        decoded = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
-    except:
-        raise HTTPException(status_code=401, detail="Not Logged In")
+    user_result = db.session.query(db.User).filter(
+        db.User.email == str(user["email"])).first()
 
-    print(decoded)
+    query = db.session.query(db.Pet).filter(db.Pet.owner_id == user_result.id)
+    result = query.filter(db.Pet.id == pet_id).first()
 
-    stored_story = dict()
-    stored_story["story"] = story.dict()
-    stored_story["email"] = decoded["email"]
+    if result is None:
+        raise HTTPException(status_code=400, detail="Pet doesn't exist!")
 
-    with open("stories.txt", "a") as f:
-        json.dump(stored_story, f, indent=2)
-        f.write("\n")
+    db_story = db.StoryRequest(owner=user_result.email, pet_id=pet_id, **story.dict())
+    db.session.add(db_story)
+    db.session.commit()
+    db.session.refresh(db_story)
 
-    return {"message": "Hello World"}
+    return Story.from_orm(db_story)
+
+
+@app.get("/story_requests")
+async def story_requests(bearer: Optional[str] = Cookie(None)):
+    user = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
+
+    result = db.session.query(db.StoryRequest).filter(
+        db.StoryRequest.owner == user["email"]).all()
+
+    if result is None:
+        raise HTTPException(status_code=404, detail="No Story Requests")
+
+    return [Story.from_orm(story) for story in result]
+
+
+@app.post("/pet")
+async def pet(pet: PetRequest, bearer: Optional[str] = Cookie(None)):
+    user = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
+
+    user_result = db.session.query(db.User).filter(
+        db.User.email == str(user["email"])).first()
+
+    query = db.session.query(db.Pet).filter(db.Pet.owner_id == user_result.id)
+    result = query.filter(db.Pet.name == pet.name).first()
+
+    if result is not None:
+        raise HTTPException(status_code=400, detail="Pet already exists")
+
+    db_pet = db.Pet(owner_id=user_result.id, **pet.dict())
+    db.session.add(db_pet)
+    db.session.commit()
+    db.session.refresh(db_pet)
+
+    return Pet.from_orm(db_pet)
+
+
+@app.get("/pet/{pet_id}")
+async def pet(pet_id: int, bearer: Optional[str] = Cookie(None)):
+    user = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
+
+    user_result = db.session.query(db.User).filter(
+        db.User.email == str(user["email"])).first()
+
+    query = db.session.query(db.Pet).filter(db.Pet.owner_id == user_result.id)
+    result = query.filter(db.Pet.id == pet_id).first()
+
+    if result is None:
+        raise HTTPException(status_code=403, detail="Unauthorized")
+
+    return Pet.from_orm(result)
+
+
+@app.patch("/pet/{pet_id}")
+async def pet(pet_id: int, pet: PetPatch, bearer: Optional[str] = Cookie(None)):
+    user = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
+
+    user_result = db.session.query(db.User).filter(
+        db.User.email == str(user["email"])).first()
+
+    query = db.session.query(db.Pet).filter(db.Pet.owner_id == user_result.id)
+    result = query.filter(db.Pet.id == pet_id).first()
+
+    if result is None:
+        raise HTTPException(status_code=400, detail="Pet doesn't exist!")
+
+    pet_updated = {
+        **Pet.from_orm(result).dict(),
+        **pet.dict(exclude_none=True)
+    }
+
+    db_pet = db.Pet(**pet_updated)
+
+    db.session.query(db.Pet).filter(
+        db.Pet.id == pet_id).update(
+            {db.Pet.photo: pet_updated["photo"], }
+    )
+    db.session.commit()
+
+    # db.session.add(db_pet)
+    # db.session.commit()
+
+    # return Pet.from_orm(db_pet)
+
+    return Pet(**pet_updated)
+
+
+@app.get("/pets")
+async def get_pet(bearer: Optional[str] = Cookie(None)):
+    user = jwt.decode(bearer, SECRET_KEY, algorithms=[ALGORITHM])
+
+    user_result = db.session.query(db.User).filter(
+        db.User.email == str(user["email"])).first()
+
+    query = db.session.query(db.Pet).filter(
+        db.Pet.owner_id == user_result.id).all()
+
+    return [Pet.from_orm(pet) for pet in query]
 
 
 @app.post("/login")
@@ -106,7 +239,22 @@ async def login(token: Token):
     idinfo = id_token.verify_oauth2_token(
         token.token, requests.Request(), CLIENT_ID, clock_skew_in_seconds=60)
 
-    print(idinfo)
+    result = db.session.query(db.User).filter(
+        db.User.email == str(idinfo["email"])).first()
+
+    # Creates a user if they don't exist
+    if result is None:
+        new_user = User(**dict({
+            "email": idinfo["email"],
+            "first_name": idinfo["given_name"],
+            "last_name": idinfo["family_name"],
+            "picture": idinfo["picture"],
+            "tokens": 0,
+        }))
+        print(new_user)
+        db_user = db.User(**new_user.dict())
+        db.session.add(db_user)
+        db.session.commit()
 
     encoded_jwt = jwt.encode({
         "email": idinfo["email"],
@@ -142,10 +290,15 @@ async def webhook(stripe_webhook: Request):
 
     email: str = data["data"]["object"]["customer_email"]
 
+    result = db.session.query(db.User).filter(
+        db.User.email == email).first()
+
+    user = User.from_orm(result)
+
     line_items = stripe.checkout.Session.list_line_items(
         data["data"]["object"]["id"], limit=5)
 
-    print(line_items)
+    # print(line_items)
 
     qty = 0
     for item in line_items["data"]:
@@ -154,11 +307,10 @@ async def webhook(stripe_webhook: Request):
         elif item["price"]["id"] == "price_1MDfW4DrFDmQr8e1TOcYTc24":
             qty = 1
 
-    print(f"{email} bought {qty} tokens")
+    print(f"{user.first_name} {user.last_name} <{user.email}> bought {qty} tokens")
 
-    if email in fake_users_db:
-        fake_users_db[email] += qty
-    else:
-        fake_users_db[email] = qty
+    query = db.session.query(db.User).filter(db.User.email == email)
+    query.update({db.User.tokens: user.tokens + qty})
+    db.session.commit()
 
     return {"message": "Webhook!"}
